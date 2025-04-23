92813848034b24fcb97e5b4fa225107fb7b0a785
LionsonM <zulomm@outlook.com>
2025-04-24 02:03:08 +0800

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,7 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        super(800, 600, 1);
         
         Character man = new Character();
         addObject(man , 100, 100);
