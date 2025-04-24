635798c27f377e9857344693c8a205d25ac23a74
LionsonM <zulomm@outlook.com>
2025-04-24 09:15:01 +0800

    move

--- FreeBird.java
+++ FreeBird.java
@@ -14,6 +14,6 @@ public class FreeBird extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+       move(-15);
     }
 }
--- GameOver.java
+++ GameOver.java
@@ -14,6 +14,6 @@ public class GameOver extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+       
     }
 }
